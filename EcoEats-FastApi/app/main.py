9db1c8b02716bc4d5ceba4dbb8fde837c68699dd
mdiallo5534-wq <mdiallo5534@gmail.com>
2025-10-08 16:16:03 +0800

    made some changes to the main

--- EcoEats-FastApi/app/main.py
+++ EcoEats-FastApi/app/main.py
@@ -1,8 +1,18 @@
 # app/main.py
 from fastapi import FastAPI
 from app.routers import auth, inventory, browse
+from fastapi.middleware.cors import CORSMiddleware
+from app.config import settings
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALLOW_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
