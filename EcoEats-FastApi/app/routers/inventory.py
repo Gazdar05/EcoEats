1ed2dbdc28a195be6da3c8aebcc4416909f952d2
mdiallo5534-wq <mdiallo5534@gmail.com>
2025-10-08 14:28:08 +0800

    made some changes

--- EcoEats-FastApi/app/routers/inventory.py
+++ EcoEats-FastApi/app/routers/inventory.py
@@ -4,6 +4,29 @@ from fastapi import APIRouter
 router = APIRouter()
 
 # TODO: Inventory endpoints will be added by teammate
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 '''from fastapi import APIRouter, HTTPException
 from app.models import FoodItem, FoodCategory
 from app.database import db
