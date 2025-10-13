65cd8527eb473acc05e6313b875942d20736caaa
Gazdar05 <zayyangazder1357@gmail.com>
2025-10-13 18:17:20 +0800

    making some backend changes

--- EcoEats-FastApi/app/config.py
+++ EcoEats-FastApi/app/config.py
@@ -1,9 +1,13 @@
-#config.py inside app
+# app/config.py
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    MONGO_URI: str = "your-mongodb-atlas-uri"
-    DB_NAME: str = "ecoeats"
+    MONGO_URI: str
+    DB_NAME: str
+    JWT_SECRET: str
+    EMAIL_SENDER: str
+    EMAIL_PASSWORD: str
+    FRONTEND_URL: str = "http://localhost:5173"
 
     class Config:
         env_file = ".env"
