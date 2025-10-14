97dc8a4a5e5c6e173067f119c37eba59bf8fbfdb
emmelxne <b2300740@helplive.edu.my>
2025-10-15 03:48:06 +0800

    update config.py

--- EcoEats-FastApi/app/config.py
+++ EcoEats-FastApi/app/config.py
@@ -1,14 +1,14 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    MONGO_URI: str = "your-mongodb-atlas-uri"
-    DB_NAME: str = "ecoeats"
+    MONGO_URI: str
+    DB_NAME: str
     JWT_SECRET: str
     EMAIL_SENDER: str
     EMAIL_PASSWORD: str
+    FRONTEND_URL: str = "http://localhost:5173"  # ✅ match .env
 
-    FRONTEND_URL: str = "http://localhost:5173"
-    class Config:
-        env_file = ".env"
 
-settings = Settings()
+    model_config = SettingsConfigDict(env_file="app/.env")  # ✅ point to correct path
+
+    settings=Settings()
