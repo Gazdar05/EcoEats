566960d499bcaf041df618adf208dc209bc77318
emmelxne <b2300740@helplive.edu.my>
2025-11-19 09:05:39 +0800

    update pie chart radius again

--- EcoEats-React/src/WebPages/Analytics-Emmeline/FoodAnalytics.tsx
+++ EcoEats-React/src/WebPages/Analytics-Emmeline/FoodAnalytics.tsx
@@ -366,7 +366,7 @@ export default function FoodAnalytics() {
                     nameKey="name"
                     cx="50%"
                     cy="50%"
-                    outerRadius={99}
+                    outerRadius={90}
                     label
                   >
                     {categoryData.map((_, index) => (
